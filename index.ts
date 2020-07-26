2dd973b44d1e4c51dc4b390e67abde73ed794bb7
Migushthe2nd <ihaverkamp@live.nl>
2020-07-26 13:03:30 +0200

    Update IDs

--- index.ts
+++ index.ts
@@ -28,30 +28,36 @@ class TwoWayMap {
 }
 
 const convertID = new TwoWayMap({
-	builtin_CarefulLayout: 'Themezer:e3f741ae-7e7a-4606-bb0b-dfdab715c0cb',
-	builtin_DiamondHome: 'Themezer:2e007a7f-51df-4b9d-a739-6ec5abcbc9a9',
-	builtin_AllApps90S: 'Themezer:1eacc544-5b04-44fc-858c-269f8f89367e',
-	builtin_AllApps90Round: 'Themezer:1eacc544-5b04-44fc-858c-269f8f89367e|2ee3f0bc-9140-402e-bdfc-d50042c4d45e',
-	builtin_DogeLayoutRound: 'Themezer:a323d9a9-7bae-4afd-912a-db9a1e37d279|7638bb49-7475-4d11-a76a-ec9d63b12b94',
-	builtin_FlowLayout: 'Themezer:0cbb94e7-df26-469b-86fa-5c788c44a518',
-	// builtin_MiniLock: '',
-	builtin_SmallCompactHomescreen: 'Themezer:152383d4-89ac-4e7c-8689-a7c272927875',
-	builtin_RoundSmallCompactHome: 'Themezer:152383d4-89ac-4e7c-8689-a7c272927875|e818c9af-ca39-48cd-8ed1-a979b69ea142',
-	builtin_SideLock: 'Themezer:1133f17a-9589-49f1-8b82-a1e7908f844f',
-	builtin_JAGLayout: 'Themezer:37fa5b25-5e90-4879-a215-81a16080c445',
-	builtin_SimpleLayoutLockscreen: 'Themezer:b388dfc8-4216-42e2-96aa-2121da42b263',
-	builtin_DogeLayout: 'Themezer:a323d9a9-7bae-4afd-912a-db9a1e37d279',
-	builtin_ClearLock: 'Themezer:f8cdb51f-1f78-49f9-9e49-79e77b6ba558',
-	builtin_TransparentPSL90SC:
-		'Themezer:c7323872-49f1-4abe-b526-c7650851fd76|b77b434f-5811-42fc-bd5e-ab44d7f24b61,d22d7557-99da-4710-a2d8-0bb3c7bfac14',
-	builtin_TransparentPSL90ST:
-		'Themezer:c7323872-49f1-4abe-b526-c7650851fd76|b77b434f-5811-42fc-bd5e-ab44d7f24b61,219eaa0a-9bbd-410c-9806-5bc898ad6849',
-	builtin_TransparentPSL90S: 'Themezer:c7323872-49f1-4abe-b526-c7650851fd76|b77b434f-5811-42fc-bd5e-ab44d7f24b61,',
-	builtin_TransparentPSLCentered:
-		'Themezer:c7323872-49f1-4abe-b526-c7650851fd76|d22d7557-99da-4710-a2d8-0bb3c7bfac14',
-	builtin_TransparentPSLTop: 'Themezer:c7323872-49f1-4abe-b526-c7650851fd76|219eaa0a-9bbd-410c-9806-5bc898ad6849',
-	builtin_TransparentPSL: 'Themezer:c7323872-49f1-4abe-b526-c7650851fd76',
+	// ResidentMenu
+	builtin_CarefulLayout: 'Themezer:00006',
+	builtin_FlowLayout: 'Themezer:00005',
+	builtin_JAGLayout: 'Themezer:00002',
+	builtin_DogeLayout: 'Themezer:0000e',
+	builtin_DogeLayoutRound: 'Themezer:0000e|7638bb49-7475-4d11-a76a-ec9d63b12b94',
+	builtin_DiamondHome: 'Themezer:0000d',
+	builtin_SmallCompactHomescreen: 'Themezer:0000f',
+	builtin_RoundSmallCompactHome: 'Themezer:0000f|e818c9af-ca39-48cd-8ed1-a979b69ea142',
+
 	// builtin_TwoRowHome: '', OUTDATED
+	// Entrance
+	builtin_ClearLock: 'Themezer:00009',
+	builtin_SimpleLayoutLockscreen: 'Themezer:0000a',
+	builtin_SideLock: 'Themezer:00001',
+	builtin_MiniLock: 'Themezer:00013',
+
+	// Flaunch
+	builtin_AllApps90S: 'Themezer:0000b',
+	builtin_AllApps90Round: 'Themezer:0000b|2ee3f0bc-9140-402e-bdfc-d50042c4d45e',
+
+	// Psl
+	builtin_TransparentPSL90SC:
+		'Themezer:00003|b77b434f-5811-42fc-bd5e-ab44d7f24b61,d22d7557-99da-4710-a2d8-0bb3c7bfac14',
+	builtin_TransparentPSL90ST:
+		'Themezer:00003|b77b434f-5811-42fc-bd5e-ab44d7f24b61,219eaa0a-9bbd-410c-9806-5bc898ad6849',
+	builtin_TransparentPSL90S: 'Themezer:00003|b77b434f-5811-42fc-bd5e-ab44d7f24b61,',
+	builtin_TransparentPSLCentered: 'Themezer:00003|d22d7557-99da-4710-a2d8-0bb3c7bfac14',
+	builtin_TransparentPSLTop: 'Themezer:00003|219eaa0a-9bbd-410c-9806-5bc898ad6849',
+	builtin_TransparentPSL: 'Themezer:00003',
 });
 
 export const parseThemeID = (ID: any) => {
